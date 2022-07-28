af3891f02b787cc01008bd8b21d1c3c96e88cf7d
Gabriela Webb <gabw813@gmail.com>
2022-07-28 14:05:36 -0700

    firebase setup and deploy

---
+++ firebase.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const config = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  // databaseURL: process.env.REACT_APP_DB,
+  projectId: process.env.REACT_APP_PID,
+  storageBucket: process.env.REACT_APP_SB,
+  messagingSenderId: process.env.REACT_APP_SID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MID,
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(config);
+const analytics = getAnalytics(app);
